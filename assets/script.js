384ad4ef7d792356cd13483775c7856f272faa38
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-02-08 23:15:19 -0500

    work distribution organized

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var genres = [];
+
 $(document).ready(function(){
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
@@ -10,7 +12,6 @@ $(document).ready(function(){
   );
       $('.parallax').parallax();
      
- 
        
        
  
@@ -18,3 +19,45 @@ $(document).ready(function(){
      
       
 });
+
+
+const clientId = '03d8e475016f40709515ff7168828110';
+const clientSecret = 'a5e76637ec5544bb88cdc130e089668d';
+var token = "";
+
+
+const _getToken = async () => {
+
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded', 
+            'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+        },
+        body: 'grant_type=client_credentials'
+    });
+        // console.log(result.access_token);
+    const data = await result.json();
+    console.log(data.access_token);
+    token = data.access_token;
+    _getGenres();
+        
+    }
+    
+const _getGenres = async () => {
+
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+        
+    const data = await result.json();
+    console.log(data);
+
+    return data.categories.items;
+}
+
+
+
+
+_getToken();
